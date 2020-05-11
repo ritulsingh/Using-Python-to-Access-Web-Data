9af9e1c2e4137a337f6cf59ce464a23bf0d27736
Ritul Singh <53478620+lutir05@users.noreply.github.com>
2020-05-11 16:55:29 +0530

    Add files via upload

---
+++ Week-3 Assignment Solution/Week-3 Assignment Solution.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 11 16:26:46 2020
+
+@author: Ritul Singh
+"""
+
+import socket
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
+
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(),end='')
+
+mysock.close()
